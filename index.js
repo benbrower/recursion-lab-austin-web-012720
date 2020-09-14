4a7c74e02c67106542a3f8ba3e42c2cd243270d5
Ben Brower <bbrower.dt@gmail.com>
2020-09-14 18:32:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // Code your solution here!
-
 function printString(str) {
   console.log(str[0])
   if (str.length > 1){
     printString(str.substring(1, str.length))
   } else { 
     return true
+  }
+}
+
+function reverseString(str){
+  if (str.length < 2){
+    return str;
+  } else {
+    console.log(str.substring(1) + str[0])
+    return reverseString(str.substring(1)) + str[0]
+    
   }
 }
